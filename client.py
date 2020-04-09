15f36b20bca331c8336179b252c574dbbc742ad0
Noble-1206 <60473607+Noble-1206@users.noreply.github.com>
2020-04-10 02:16:00 +0800

    Update client.py

--- client.py
+++ client.py
@@ -1,55 +1,85 @@
 import pygame
+from network import network
 
 width = 500
 height = 500
-
 win = pygame.display.set_mode((width, height))
-pygame.display.set_caption("Manhattan")
+pygame.display.set_caption("Client")
 
 clientNumber = 0
 
-class Player(object):
+
+class Player():
     def __init__(self, x, y, width, height, color):
         self.x = x
         self.y = y
         self.width = width
         self.height = height
         self.color = color
-        self.vel = 5
-        self.rect = (x, y, width, height)
+        self.rect = (x,y,width,height)
+        self.vel = 3
 
     def draw(self, win):
         pygame.draw.rect(win, self.color, self.rect)
 
     def move(self):
         keys = pygame.key.get_pressed()
+
         if keys[pygame.K_LEFT]:
             self.x -= self.vel
+
         if keys[pygame.K_RIGHT]:
             self.x += self.vel
+
         if keys[pygame.K_UP]:
             self.y -= self.vel
+
         if keys[pygame.K_DOWN]:
             self.y += self.vel
 
-            self.rect = (self.x, self.y, self.width, self.height)
+        self.update()
 
-def redrawWindow(win, player):
-    win.fill((255, 255, 255))
+    def update(self):
+        self.rect = (self.x, self.y, self.width, self.height)
+
+
+def read_pos(str):
+    str = str.split(",")
+    return int(str[0]), int(str[1])
+
+
+def make_pos(tup):
+    return str(tup[0]) + "," + str(tup[1])
+
+
+def redrawWindow(win,player, player2):
+    win.fill((255,255,255))
     player.draw(win)
-    pygame.display.update() #업데이트 
-    
+    player2.draw(win)
+    pygame.display.update()
+
+
 def main():
-    p1 = Player(50, 50, 100, 100, (10, 100, 100))
     run = True
+    n = network()
+    startPos = read_pos(n.getPos())
+    p = Player(startPos[0],startPos[1],100,100,(0,255,0))
+    p2 = Player(0,0,100,100,(255,0,0))
     clock = pygame.time.Clock()
+
     while run:
         clock.tick(60)
+        p2Pos = read_pos(n.send(make_pos((p.x, p.y))))
+        p2.x = p2Pos[0]
+        p2.y = p2Pos[1]
+        p2.update()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 pygame.quit()
-        p1.move()
-        redrawWindow(win, p1)
+
+        p.move()
+        redrawWindow(win, p, p2)
 
 main()
