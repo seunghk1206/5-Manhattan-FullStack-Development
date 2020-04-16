bb40e47ef19375bae8389095c5ad4b72f7529ed4
Noble-1206 <60473607+Noble-1206@users.noreply.github.com>
2020-04-17 00:01:01 +0800

    Create networkRPS.py

---
+++ networkRPS.py
@@ -0,0 +1,27 @@
+import socket
+import pickle
+
+class network():
+    def __init__(self):
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server = ""
+        self.port = 5555
+        self.addr = (self.server, self.port) #ip 주소, 포트 --> 튜플
+        self.p = self.connect() # id = connect를 통한 return 값
+
+    def getP(self):
+        return self.p
+
+    def connect(self):
+        try:
+            self.client.connect(self.addr) #
+            return self.client.recv(2048).decode()
+        except:
+            pass
+
+    def send(self, data):
+        try: 
+            self.client.send(str.encode(data))
+            return pickle.loads(self.client.recv(2048))
+        except socket.error as e:
+            print(e)
